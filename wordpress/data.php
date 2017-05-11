17b5707d98e4bc2b2f8373f2cf965e41c3b6b3df
georgievtodor <todorgeorgiev96@gmail.com>
2017-05-11 17:20:22 +0300

    displaying google map and feeding data from mysql

---
+++ wordpress/data.php
@@ -0,0 +1,40 @@
+<?php
+    $con = @new mysqli('localhost', 'user', 'asdf42', 'wordpress');
+
+    if($con->connect_error) {
+        echo "error: " . $con->connect_error;
+        exit();
+    }
+
+    $query = "SELECT * FROM wp_wpgmza";
+
+    $result = $con->query($query);
+    if($result !== FALSE) {
+        $result = $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+   mysqli_close($con);
+
+   $doc = new DOMDocument('1.0', 'UTF-8');
+
+   $xmlRoot = $doc->createElement("markers");
+   $xmlRoot = $doc->appendChild($xmlRoot);
+   header("Content-type: text/xml");
+   foreach($result as $item) {
+    // var_dump($item);
+    // debug($item['lat']);
+    // debug($item['lng']);
+    $node = $doc->createElement('marker');
+    $newNode = $xmlRoot->appendChild($node);
+    $newNode->setAttribute('lat', $item['lat']);
+    $newNode->setAttribute('lng', $item['lng']);
+   }
+
+//    debug(json_encode($result));
+
+    function debug($msg) {
+        $msg = str_replace('"', '\\"', $msg);
+        echo "<script>console.log(\"$msg\")</script>";
+    }
+
+    echo $doc->saveXML();
--- wordpress/index.php
+++ wordpress/index.php
@@ -1,17 +1,124 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+    <html lang="en">
+            <head> 
+            <meta http-equiv="content-type" content="text/html; charset=utf-8"> 
+            <title>Title Goes Here</title> 
+            <style>
+            #map {
+                width:100%;
+                height: 500px;
+            }
+            </style>
+            </head> 
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+            <body>
+                <div id="map"></div>
+            </body> 
+            <footer>
+            <!--<script type="text/javascript" src="http://api.eventful.com/js/api">
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+            </script>
+
+            <script type="text/javascript">
+
+            function show_alert()
+
+            {
+
+            var oArgs = {
+
+                        app_key:"ZF8DQx6LN9zKpNPp",
+
+                        date: "Last Week",
+
+                        page_size: 15 ,
+
+            };
+
+            EVDB.API.call("/events/search", oArgs, function(oData) {
+                console.log(oData.events.event);
+                // Note: this relies on the custom toString() methods below
+
+                });
+
+            }
+
+            show_alert();
+
+            </script>-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <script>
+                    function initMap() {
+                        downloadUrl("http://telebid.pro/test/wordpress/data.php", function(data) {
+                            console.log(data);
+                            console.log(data.responseXML)
+                            var xml = data.responseXML;
+                            var markers = xml.documentElement.getElementsByTagName('marker');
+                            Array.prototype.forEach.call(markers, function(elem) {
+                               var point = new google.maps.LatLng(
+                                   parseFloat(elem.getAttribute('lat')),
+                                   parseFloat(elem.getAttribute('lng'))
+                               );
+
+                               var marker = new google.maps.Marker({
+                                   map: map,
+                                   position: point
+                               });
+                            });
+                        })
+
+                        var centerCoords = new google.maps.LatLng(42.6977, 23.3219)
+
+                        var map = new google.maps.Map(document.getElementById('map'), {
+                            center: centerCoords,
+                            zoom: 3
+                        });
+                        console.log(map)
+                        var infoWindow = new google.maps.InfoWindow;
+
+                        var marker = new google.maps.Marker({
+                            position: centerCoords,
+                            map: map
+                        });
+                    }
+
+                    function downloadUrl(url, callback) {
+                    var request = window.ActiveXObject ?
+                        new ActiveXObject('Microsoft.XMLHTTP') :
+                        new XMLHttpRequest;
+
+                    request.onreadystatechange = function() {
+                        if (request.readyState == 4) {
+                            request.onreadystatechange = doNothing;
+                            callback(request, request.status);
+                        }
+                    };
+
+                    request.open('GET', url, true);
+                    request.send(null);
+                }
+
+                function doNothing() {
+
+                }
+
+
+                </script>
+                <script async defer
+                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDZtHMW4dtJH2YQyLHWD8Dyub0fTkdGXPM&callback=initMap">
+                </script>
+
+            </footer>
+    </html> 
